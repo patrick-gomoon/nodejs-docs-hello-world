97d1c974fc7e66a08976605d5d8da4ab3ae697c8
pa201608 <patrick@gomoon.io>
2023-05-28 17:10:07 +0200

    Rename web app again

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
-  name                = "webapp-asp-node-app-${random_integer.ri.result}"
+  name                = "webapp-asp-node-app-helloworld" #${random_integer.ri.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
@@ -35,7 +35,7 @@ resource "azurerm_service_plan" "appserviceplan" {
 
 # Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "webapp" {
-  name                = "webapp-node-app-${random_integer.ri.result}"
+  name                = "webapp-node-app-helloworld" #${random_integer.ri.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.appserviceplan.id
@@ -45,11 +45,11 @@ resource "azurerm_linux_web_app" "webapp" {
   }
 }
 
-#  Deploy code from a public GitHub repo
-resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id                 = azurerm_linux_web_app.webapp.id
-  repo_url               = "https://github.com/patrick-gomoon/nodejs-docs-hello-world.git"
-  branch                 = "main"
-  use_manual_integration = true
-  use_mercurial          = false
-}
+# #  Deploy code from a public GitHub repo
+# resource "azurerm_app_service_source_control" "sourcecontrol" {
+#   app_id                 = azurerm_linux_web_app.webapp.id
+#   repo_url               = "https://github.com/patrick-gomoon/nodejs-docs-hello-world.git"
+#   branch                 = "main"
+#   use_manual_integration = true
+#   use_mercurial          = false
+# }
